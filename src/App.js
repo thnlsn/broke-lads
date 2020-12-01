5e0801a7b8070fc71c9f8d1c6496b0563f4cda3e
Thomas Nelson <thnlsn@gmail.com>
2020-11-30 21:31:02 -0800

    basic structure in place, not yet seperated into components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './css/style.css';
 
 function App() {
-  return <div className='App'>Broke Lads</div>;
+  return (
+    <div className='App'>
+      <Fragment>
+        <header className='header'>
+          Header
+          <nav className='navigation'>Nav</nav>
+        </header>
+        <main className='content'>Content</main>
+        <footer className='footer'>Footer</footer>
+      </Fragment>
+    </div>
+  );
 }
 
 export default App;
