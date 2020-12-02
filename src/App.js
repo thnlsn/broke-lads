f277d015d5cc3d9a37e21a12fa14fd42f71c7831
Thomas Nelson <thnlsn@gmail.com>
2020-12-02 11:28:56 -0800

    attached the background and made it fixed so that the elements on the page scroll over it

--- src/App.js
+++ src/App.js
@@ -9,21 +9,23 @@ function App() {
     <div className='App'>
       <Fragment>
         <header className='header'>
-          <div className='header__logo-box'>
-            <img src={headerLogo} alt='Logo' className='header__logo' />
-          </div>
-          <div className='header__text-box'>
-            <h1 className='heading-primary'>
-              <span className='heading-primary--main'>Broke Lads</span>
-              <span className='heading-primary--sub'>
-                "Savages with empty pockets"
-              </span>
-            </h1>
+          <div className='title'>
+            <div className='title__logo-box'>
+              <img src={headerLogo} alt='Logo' className='header__logo' />
+            </div>
+            <div className='title__text-box'>
+              <h1 className='heading-primary'>
+                <span className='heading-primary--main'>Broke Lads</span>
+                <span className='heading-primary--sub'>
+                  "Savages with empty pockets"
+                </span>
+              </h1>
+            </div>
           </div>
         </header>
         <main className='content'>
           <nav className='navigation'>Nav</nav>
-          Content
+          <div className='heading-secondary'>Who are we?</div>
         </main>
         <footer className='footer'>Footer</footer>
       </Fragment>
