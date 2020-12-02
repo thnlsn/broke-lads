54bc2c1a140266d59f72d238537869ac7e73dace
Thomas Nelson <thnlsn@gmail.com>
2020-12-01 16:57:12 -0800

    some basic layout for the contents of the header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Fragment } from 'react';
 import './css/style.css';
 
+// Logo image for the header
+const headerLogo = '';
+
 function App() {
   return (
     <div className='App'>
       <Fragment>
         <header className='header'>
-          Header
-          <nav className='navigation'>Nav</nav>
+          <div className='header__logo-box'>
+            <img src={headerLogo} alt='Logo' className='header__logo' />
+          </div>
+          <h1 className='heading-primary'>
+            <span className='heading-primary--main'>Broke Lads</span>
+            <span className='heading-primary--sub'>
+              "Savages with empty pockets"
+            </span>
+          </h1>
         </header>
-        <main className='content'>Content</main>
+        <main className='content'>
+          <nav className='navigation'>Nav</nav>
+          Content
+        </main>
         <footer className='footer'>Footer</footer>
       </Fragment>
     </div>
