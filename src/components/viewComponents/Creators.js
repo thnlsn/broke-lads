6afc36fe941bb8aff0cb1b9f0c9c4de81939f0e9
Thomas Nelson <thnlsn@gmail.com>
2020-12-06 02:46:28 -0800

    quick mockup of the creator tab content for client demo tomorrow

---
+++ src/components/viewComponents/Creators.js
@@ -0,0 +1,60 @@
+import React, { Fragment } from 'react';
+
+const Socials = () => {
+  return (
+    <Fragment>
+      <div className='creators'>
+        <div className='heading-secondary u-margin-bottom-medium'>Our Work</div>
+
+        <div className='work'>
+          <div className='paragraph paragraph--lined work__container'>
+            <p className='work__title'>
+              <span className='emphasis'>The Void</span>
+            </p>
+            <p className='work__description'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+              quaerat obcaecati non odio quia? Consectetur, odio dolores
+              voluptatibus dolorum harum ut voluptates ad, sit, dicta rem non
+              alias iste atque!
+            </p>
+          </div>
+          <div className='paragraph paragraph--lined work__container'>
+            <p className='work__title'>
+              <span className='emphasis'>Paper Sketch</span>
+            </p>
+            <p className='work__description'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos alias
+              et sunt fugit voluptatum placeat impedit dolorem omnis, unde
+              veritatis a voluptate deserunt! Inventore quod, mollitia iure
+              facilis eveniet est.
+            </p>
+          </div>
+          <div className='paragraph paragraph--lined work__container'>
+            <p className='work__title'>
+              <span className='emphasis'>DC Comics</span>
+            </p>
+            <p className='work__description'>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio
+              voluptatum voluptatibus quas temporibus minima nisi quia dolores
+              dolorem, rerum quae eveniet porro neque perspiciatis placeat
+              numquam voluptates non sit molestias?
+            </p>
+          </div>
+          <div className='paragraph paragraph--lined work__container'>
+            <p className='work__title'>
+              <span className='emphasis'>Marvel Comics</span>
+            </p>
+            <p className='work__description'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+              dolores reprehenderit vitae maiores voluptatem hic quos porro
+              laboriosam iure repellat! Architecto eos voluptates laudantium
+              autem dolores quisquam ab odio.
+            </p>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Socials;
